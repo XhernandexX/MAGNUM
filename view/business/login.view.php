10041a37203ec877ee98d53f73a8d6f5a328522b
XhernandexX <camilohernandez351@gmail.com>
2022-10-21 16:39:27 -0500

    Conexión Base de Datos Clever

--- view/business/login.view.php
+++ view/business/login.view.php
@@ -21,33 +21,29 @@
 
 <div class="col-md-7 ">
 
-    <form method="POST" enctype="multipart/form-data">
-
-	<form action="?c=Login&a=validarLogin" method="post">
-					<div class="email mb-4">
-						<label class="sr-only" for="usuario">Usuario:</label>
-						<input id="usuario" name="usuario" type="text" class="form-control usuario" placeholder="Usuario" required="required">
+    <form action="?c=Login&a=validarLogin" method="post">
+		<div class="email mb-4">
+			<label class="sr-only" for="usuario">Usuario:</label>
+			<input id="usuario" name="usuario" type="text" class="form-control usuario" placeholder="Usuario" required="required">
+		</div>
+		<div class="password mb-4">
+			<label class="sr-only" for="contrasenia">Contraseña:</label>
+			<input id="contrasenia" name="contrasenia" type="password" class="form-control contrasenia" placeholder="Contraseña" required="required">
+			<div class="extra mt-3 row justify-content-between">
+				<div class="col-6">
+					<div class="form-check">
+						<input class="form-check-input" type="checkbox" value="" id="RememberPassword">
+						<label class="form-check-label" for="RememberPassword">
+							Recuerdame
+						</label>
 					</div>
-					<div class="password mb-4">
-						<label class="sr-only" for="contrasenia">Contraseña:</label>
-						<input id="contrasenia" name="contrasenia" type="password" class="form-control contrasenia" placeholder="Contraseña" required="required">
-						<div class="extra mt-3 row justify-content-between">
-							<div class="col-6">
-								<div class="form-check">
-									<input class="form-check-input" type="checkbox" value="" id="RememberPassword">
-									<label class="form-check-label" for="RememberPassword">
-										Recuerdame
-									</label>
-								</div>
-							</div>
-							<div class="col-6"></div>
-						</div>
-					</div>
-					<div class="text-center">
-						<button type="submit" class="btn btn-light">Iniciar Sesión</button>
-					</div>
-				</form>
-
+				</div>
+				<div class="col-6"></div>
+			</div>
+		</div>
+		<div class="text-center">
+			<button type="submit" class="btn btn-light">Iniciar Sesión</button>
+		</div>
     </form>
 
 </div>
