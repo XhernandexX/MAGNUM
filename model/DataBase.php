50afe653ec15eae779299f8972707b09f9a99a88
XhernandexX <camilohernandez351@gmail.com>
2022-10-21 16:25:51 -0500

    Conexión Base de Datos Clever

--- model/DataBase.php
+++ model/DataBase.php
@@ -2,7 +2,7 @@
 	class DataBase{		
 		public function __construct(){}
 		public static function conexion(){
-			$pdo = new PDO('mysql:host=localhost;dbname=proyecto_magnun_bbdd;charset=utf8','root','');
+			$pdo = new PDO('mysql:host=bq36b1laj9tspvtyfk0a-mysql.services.clever-cloud.com;dbname=bq36b1laj9tspvtyfk0a;charset=utf8','uextwwe1soc1adhw','0EyhOqBvwyFjCE7HWZTh');
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
 		}
