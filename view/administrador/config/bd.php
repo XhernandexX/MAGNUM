52f224ed5dea600a980eb9e9fef4cd0072e57f9f
XhernandexX <camilohernandez351@gmail.com>
2022-10-24 10:28:48 -0500

    Base de Datos

--- view/administrador/config/bd.php
+++ view/administrador/config/bd.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost";
-$bd="mgnm";
-$usuario="root";
-$contrasenia="";
+$host="bq36b1laj9tspvtyfk0a-mysql.services.clever-cloud.com";
+$bd="bq36b1laj9tspvtyfk0a";
+$usuario="uextwwe1soc1adhw";
+$contrasenia="0EyhOqBvwyFjCE7HWZTh";
 
 try {
         $conexion=new PDO("mysql:host=$host;dbname=$bd",$usuario,$contrasenia);    
